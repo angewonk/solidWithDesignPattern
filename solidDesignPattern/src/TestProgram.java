981e5f84298025d0e613dd8d69b8e1afdac23023
angewonk <angelomallari1123@gmail.com>
2024-05-12 16:19:52 +0800

    updated

--- solidDesignPattern/src/TestProgram.java
+++ solidDesignPattern/src/TestProgram.java
@@ -13,8 +13,8 @@ public class TestProgram {
         Student stud2 = new Student("Ramona Flowers");
         Student stud3 = new Student("Liza Soberano");
 
-        Resources manga = new Resources("book", "Jujutsu Kaisen");
-        Resources dictionary = new Resources("journal", "The Diary of a Young Girl by Anne Frank.");
+        Resources book = new Resources("book", "Jujutsu Kaisen");
+        Resources journal = new Resources("journal", "The Diary of a Young Girl by Anne Frank.");
         Resources comic = new Resources("comic", "The Amazing Spiderman");
         Resources biography = new Resources("biography", "Biography of Jose Rizal");
 
@@ -24,9 +24,9 @@ public class TestProgram {
         System.out.println();
         System.out.println("   ======= Welcome to the Library System =======");
         System.out.println();
-        resourceProcessor.resourceBorrower(stud1, manga);
+        resourceProcessor.resourceBorrower(stud1, book);
         System.out.println();
-        resourceProcessor.resourceBorrower(stud2, dictionary);
+        resourceProcessor.resourceBorrower(stud2, journal);
         System.out.println();
         resourceProcessor.resourceBorrower(stud2, comic);
         System.out.println();
